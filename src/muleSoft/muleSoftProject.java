0825b763f37c69bd9b2e14669a39253c534e72f3
BinnyJ <binnyjohn39@gmail.com>
2020-10-31 13:01:08 -0400

    first git message

---
+++ src/muleSoft/muleSoftProject.java
@@ -0,0 +1,9 @@
+package muleSoft;
+
+public class muleSoftProject {
+    public static void main(String[] args) {
+        System.out.println("Love you all so much");
+        System.out.println("Java is Fun");
+        System.out.println("We all are hard working people");
+    }
+}
