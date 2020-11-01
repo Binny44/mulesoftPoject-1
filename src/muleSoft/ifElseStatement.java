0ea6be4a89ddf2e623cd7e9afccb94897ff54a9b
BinnyJ <binnyjohn39@gmail.com>
2020-11-01 17:25:33 -0500

    new update files

---
+++ src/muleSoft/ifElseStatement.java
@@ -0,0 +1,14 @@
+package muleSoft;
+
+public class ifElseStatement {
+    public static void main(String[] args) {
+        int firstNum= 53;
+        int secondNum = 28;
+        int sum = firstNum * secondNum;
+        int totNum= firstNum + secondNum + sum;
+        System.out.println("Total number of int is : " + totNum);
+        System.out.println("Binny is great and smart: " + totNum + secondNum);
+        System.out.println("The Fine for two: ");
+
+    }
+}
--- src/muleSoft/muleSoftProject.java
+++ src/muleSoft/muleSoftProject.java
@@ -2,8 +2,14 @@ package muleSoft;
 
 public class muleSoftProject {
     public static void main(String[] args) {
-        System.out.println("Love you all so much");
-        System.out.println("Java is fun");
+        System.out.println("Love all Mule and QA Students");
+        System.out.println("Java is fun to learn");
+        System.out.println("Mule soft and QA are part of class");
+        System.out.println("The more you learn, the more you solve");
+        System.out.println("Fight to win");
+        System.out.println("The more you know the more");
+        System.out.println("Hello word");
+        System.out.println("The more you learn, the more you solve");
 
     }
 }
